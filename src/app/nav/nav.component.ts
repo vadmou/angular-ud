91ce283f689f0fbc5316ccc216099aa7d7c444bd
Vadym Borovok <vadym.borovok@gmail.com>
2019-11-20 08:39:38 +0200

    use *ngIf (login, logout)

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,4 +19,12 @@ export class NavComponent implements OnInit {
       console.log('Failed to login');
     });
   }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
